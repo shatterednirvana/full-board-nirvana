852993ba0398e0818f25a96440c2d9ca67553de6
Chris Bunch <shatterednirvana@gmail.com>
2014-01-13 11:02:01 -0800

    sketched out rest of initial version of program

--- lib/full_board_nirvana/cli.ex
+++ lib/full_board_nirvana/cli.ex
@@ -10,7 +10,9 @@ defmodule FullBoardNirvana.CLI do
   def run(argv) do
     argv
       |> parse_args
-      |> process
+      |> print_welcome_message
+      |> quiz_user
+      |> generate_report
   end
 
   @doc """
@@ -40,7 +42,7 @@ defmodule FullBoardNirvana.CLI do
   @doc """
   Prints the usage for this program and exits.
   """
-  def process(:help) do
+  def print_welcome_message(:help) do
     IO.puts """
     usage: full_board_nirvana [rounds | #{@default_rounds}]
     """
@@ -48,11 +50,51 @@ defmodule FullBoardNirvana.CLI do
   end
 
   @doc """
-  Quizzes the user about their chess visualization, prints summary statistics,
-  and exits.
-  """
-  def process(_rounds) do
+  Prints information about how users indicate that a square is either black or
+  white.
 
+  Returns the number of questions (rounds) that will be asked.
+  """
+  def print_welcome_message(rounds) do
+    IO.puts """
+    Hello! This program will pick a random square on the chessboard #{rounds}
+    times. It will ask you if the square is black or white. You can indicate
+    that the square is black by entering in "black", "b", or "1". You can
+    indicate that the square is white by entering in "white", "w", or "2".
+
+    Let's begin!
+    """
+    rounds
+  end
+
+  @doc """
+  Asks the user a series of questions about whether random squares on a
+  chessboard are white or black.
+
+  Returns {correct_answers, total_questions}. In the future we
+  should consider expanding this to return more detailed information. In
+  particular, we could gain more insight by returning a list, where each item
+  indicates if the question was answered correctly or not, what square was
+  chosen, and how long it took the user to respond.
+  """
+  def quiz_user(total_questions) do
+    {0, total_questions}
+  end
+
+  @doc """
+  Prints information about how many questions the user got right.
+
+  In the future, this may be expanded to include more intelligent information,
+  such as insights into specific parts of the board that the user has strong
+  or weak visualization on (also possibly taking into account how long it takes
+  to get a correct answer).
+  """
+  def generate_report({correct_answers, total_questions}) do
+    percentage = correct_answers / total_questions
+    IO.puts """
+    You successfully answered #{correct_answers} out of #{total_questions}
+    questions (#{percentage}%).
+    """
   end
 
 end
--- test/cli_test.exs
+++ test/cli_test.exs
@@ -1,7 +1,7 @@
 defmodule CliTest do
   use ExUnit.Case
 
-  import FullBoardNirvana.CLI, only: [parse_args: 1]
+  import FullBoardNirvana.CLI, only: [parse_args: 1, print_welcome_message: 1]
 
   test ":help returned by option parsing with -h and --help options" do
     assert parse_args(["-h", "anything"]) == :help
@@ -14,6 +14,10 @@ defmodule CliTest do
 
   test "count is defaulted if not provided by the user" do
     assert parse_args([]) == 25
+  end
+
+  test "print_welcome_message returns number of rounds given" do
+    assert print_welcome_message(20) == 20
   end
 
 end
