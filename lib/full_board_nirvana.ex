3d25c9d7fd50aae1f15ed3bdd60e47475105518c
Chris Bunch <shatterednirvana@gmail.com>
2014-01-13 09:37:02 -0800

    generated project with 'mix new'

---
+++ lib/full_board_nirvana.ex
@@ -0,0 +1,9 @@
+defmodule FullBoardNirvana do
+  use Application.Behaviour
+
+  # See http://elixir-lang.org/docs/stable/Application.Behaviour.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    FullBoardNirvana.Supervisor.start_link
+  end
+end
---
+++ test/full_board_nirvana_test.exs
@@ -0,0 +1,7 @@
+defmodule FullBoardNirvanaTest do
+  use ExUnit.Case
+
+  test "the truth" do
+    assert(true)
+  end
+end
